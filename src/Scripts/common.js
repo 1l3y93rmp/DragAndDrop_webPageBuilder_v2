1bba5312de583051119cb828fbef6396383efe6d
1l3y93rmp <1l3y93rmp@gmail.com>
2018-05-18 16:32:33 +0800

    橫向新增DIV (有紀錄巢狀 / 無紀錄順序 單純加再後面)

--- src/Scripts/common.js
+++ src/Scripts/common.js
@@ -20,7 +20,7 @@ $(function () {
       super()
       // contentJson 縮寫成 cJ
       this.state = {
-        cJ: [{cJ: [{cJ: []}, {cJ: []}]}, {cJ: []}]
+        cJ: []
       }
       this.dropped = this.dropped.bind(this)
     }
@@ -30,20 +30,31 @@ $(function () {
         let divStyle = {'width': 100 / cJdata.length + '%'}
         if (node.cJ.length) { // 如果node的cJ 裡面還是有東西 靠遞迴自己叫自己
           return (
-            <div key={index} style={divStyle}>
+            <div
+              key={index}
+              style={divStyle}
+              onDrop={this.dropped}
+              onDragEnter={this.cancelDefault}
+              onDragOver={this.cancelDefault}
+            >
               {this.mapToCreatDiv(node.cJ)}
             </div>
           )
         } else {
           return (
-            <div key={index} style={divStyle} />
+            <div
+              key={index}
+              style={divStyle}
+              onDrop={this.dropped}
+              onDragEnter={this.cancelDefault}
+              onDragOver={this.cancelDefault}
+            />
           )
         }
       })
     }
 
     ondragstart (e) { // 被綁在被拖的東西的 (#menu內的)
-      console.log('Yeeee')
       e.dataTransfer.setData('text/plain', e.target.id)
     }
 
@@ -55,8 +66,29 @@ $(function () {
 
     dropped (e) { // 被綁在要被放東西進來的框框(operatingArea) onDrop時調用這個方法
       console.log('進來惹')
+      console.log($(e.currentTarget).parents())
+
+      let divLevels = $(e.currentTarget).parents().length - 4// 得知該DIV往外有幾層
+      let divIndex = $(e.currentTarget).prevAll().length // 得知該DIV再同輩順位幾
       let newCj = this.state.cJ
-      newCj.push({cJ: []})
+      let divJson = {cJ: []}
+
+      if (divLevels < 0) {
+        newCj.push(divJson)
+      } else {
+        let S = '' // 空字串
+
+        for (var i = 0; i < divLevels; i++) {
+          // 從2開始是因為 Html Body #content 這三層不要
+          // 爸爸的順序是...?
+          let papaLength = $(e.currentTarget).parents().eq(i).prevAll().length
+          S = S + '[' + papaLength + '].cJ'
+        }
+
+        console.log('newCj' + S + '[' + divIndex + '].cJ.push(divJson)')
+        eval('newCj' + S + '[' + divIndex + '].cJ.push(divJson)')
+      }
+
       this.setState({cJ: newCj})
       this.cancelDefault(e)
     }
@@ -70,6 +102,7 @@ $(function () {
             <div draggable='true' onDragStart={this.ondragstart}> 添加一個框框 </div>
           </div>
           <div id='operatingArea'
+            data-role='drag-drop-container'
             onDrop={this.dropped}
             onDragEnter={this.cancelDefault}
             onDragOver={this.cancelDefault}
