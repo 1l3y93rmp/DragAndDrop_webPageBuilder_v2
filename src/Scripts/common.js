179db540b45cccac262452085307dbd5300826f3
1l3y93rmp <1l3y93rmp@gmail.com>
2018-05-18 18:35:28 +0800

    加入css reset / 判斷被加入東西框的位置 開始與游標進行比對

--- src/Scripts/common.js
+++ src/Scripts/common.js
@@ -61,24 +61,31 @@ $(function () {
       e.dataTransfer.setData('text/plain', e.target.id)
     }
 
-    cancelDefault (e) { // 在防止預設行為的方法，被dropped調用
+    cancelDefault (e) { // 在防止預設行為的方法，被 dropped dragoverGoYellow 調用
       e.preventDefault()
       e.stopPropagation()
       return false
     }
 
-    dragoverGoYellow (e) { // 被拉到的時候變成黃色ㄅ
+    dragoverGoYellow (e) { // 被拉到的時候變成黃色
       $(e.currentTarget).addClass('Yellow')
+      // 需要得知游標的位置靠近哪裡? 上/下/左/右
+      var $target = $(e.currentTarget)
+      let offset = $target.offset()
+      let $bottom = offset.top + $target.innerHeight()
+      let $right = offset.left + $target.innerWidth()
+      console.log(offset.top, $right, $bottom, offset.left)
       this.cancelDefault(e)
     }
 
-    dragleaveGoBack (e) { // 沒有被拉到的時候 變回白色
+    dragleaveGoBack (e) { // leave的時候 變回白色
       $(e.currentTarget).removeClass('Yellow')
     }
 
-    dropped (e) { // 被綁在要被放東西進來的框框(operatingArea) onDrop時調用這個方法
+    dropped (e) { // 被綁在要被放東西進來的框框(#operatingArea) onDrop時調用這個方法
       $(e.currentTarget).removeClass('Yellow')
       let divLevels = $(e.currentTarget).parents().length - 4// 得知該DIV往外有幾層
+      // -4 是因為 Html Body #content #operatingArea 這4層不要
       let divIndex = $(e.currentTarget).prevAll().length // 得知該DIV再同輩順位幾
       let newCj = this.state.cJ
       let divJson = {cJ: []}
@@ -89,7 +96,6 @@ $(function () {
         let S = '' // 空字串
 
         for (var i = 0; i < divLevels; i++) {
-          // 從2開始是因為 Html Body #content 這三層不要
           // 爸爸的順序是...?
           let papaLength = $(e.currentTarget).parents().eq(i).prevAll().length
           S = S + '[' + papaLength + '].cJ'
