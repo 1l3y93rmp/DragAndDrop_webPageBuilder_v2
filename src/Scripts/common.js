a68be7d417acb5588b6330a44899d6837c394734
1l3y93rmp <1l3y93rmp@gmail.com>
2018-05-18 11:45:23 +0800

    使 JS 隨資料改變渲染的巢狀 (仍未做出控制鈕)

--- src/Scripts/common.js
+++ src/Scripts/common.js
@@ -15,41 +15,43 @@ $(function () {
     }
   }
 
-  class BlockH2 extends React.Component {
-    // h2代表 水平 兩塊Div
-    render () {
-      return (
-        <div>
-          <DataInput class={'Horizontal'} />
-          <DataInput class={'Horizontal'} />
-        </div>
-      )
-    }
-  }
-  class BlockV2 extends React.Component {
-    // h2代表 水平 兩塊Div
-    render () {
-      return (
-        <div>
-          <DataInput class={'vertical'} />
-          <DataInput class={'vertical'} />
-        </div>
-      )
-    }
-  }
-
   class MenuAndOperatingArea extends React.Component {
+    constructor () {
+      super()
+      // contentJson 縮寫成 cJ
+      this.state = {
+        cJ: [{cJ: [{cJ: []}, {cJ: []}]}, {cJ: []}]
+      }
+    }
+
+    mapToCreatDiv (cJdata) {
+      return cJdata.map((node, index) => {
+        let divStyle = {'width': 100 / cJdata.length + '%'}
+        if (node.cJ.length) { // 如果node的cJ 裡面還是有東西 靠遞迴自己叫自己
+          return (
+            <div key={index} style={divStyle}>
+              {this.mapToCreatDiv(node.cJ)}
+            </div>
+          )
+        } else {
+          return (
+            <div key={index} style={divStyle} />
+          )
+        }
+      })
+    }
+
     render () {
+      // render operatingArea 時 會參照 State 內的資料
+
       return (
         <React.Fragment>
           <div id='menu'>
-            <div id='menuBlock'>
-              <BlockH2 />
-              <BlockV2 />
-            </div>
-            <div id='menuData' />
+            <div> 添加一個框框 </div>
           </div>
-          <div id='operatingArea' />
+          <div id='operatingArea'>
+            {this.mapToCreatDiv(this.state.cJ)}
+          </div>
         </React.Fragment>
       )
     }
