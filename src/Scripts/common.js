4cacd15802513edbebac01759a46598f575fe26c
1l3y93rmp <1l3y93rmp@gmail.com>
2018-05-18 13:59:05 +0800

    新增 拖曳動作 (尚未指定拖曳截點)

--- src/Scripts/common.js
+++ src/Scripts/common.js
@@ -22,6 +22,7 @@ $(function () {
       this.state = {
         cJ: [{cJ: [{cJ: []}, {cJ: []}]}, {cJ: []}]
       }
+      this.dropped = this.dropped.bind(this)
     }
 
     mapToCreatDiv (cJdata) {
@@ -41,15 +42,38 @@ $(function () {
       })
     }
 
+    ondragstart (e) { // 被綁在被拖的東西的 (#menu內的)
+      console.log('Yeeee')
+      e.dataTransfer.setData('text/plain', e.target.id)
+    }
+
+    cancelDefault (e) { // 在防止預設行為的方法，被dropped調用
+      e.preventDefault()
+      e.stopPropagation()
+      return false
+    }
+
+    dropped (e) { // 被綁在要被放東西進來的框框(operatingArea) onDrop時調用這個方法
+      console.log('進來惹')
+      let newCj = this.state.cJ
+      newCj.push({cJ: []})
+      this.setState({cJ: newCj})
+      this.cancelDefault(e)
+    }
+
     render () {
       // render operatingArea 時 會參照 State 內的資料
-
+      console.log(this.state.cJ)
       return (
         <React.Fragment>
           <div id='menu'>
-            <div> 添加一個框框 </div>
+            <div draggable='true' onDragStart={this.ondragstart}> 添加一個框框 </div>
           </div>
-          <div id='operatingArea'>
+          <div id='operatingArea'
+            onDrop={this.dropped}
+            onDragEnter={this.cancelDefault}
+            onDragOver={this.cancelDefault}
+          >
             {this.mapToCreatDiv(this.state.cJ)}
           </div>
         </React.Fragment>
